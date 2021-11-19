2f69c029f0b45fd86b664b932bfe9f21cd37fa7e
Asad Ullah Khan <asad.ullah.khan@macys.com>
2021-11-19 17:46:37 +0500

    added lamda for update keys of appsync after specifed time

--- ec2.py
+++ ec2.py
@@ -1,13 +1,15 @@
 import boto3
-instance_name_to_terminaite = 'testing-123'
+instance_name_to_terminaite = 'poc'
 region='eu-west-1'
 ec2 = boto3.resource('ec2',region_name=region)
 ec2_client = boto3.client('ec2')
-instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']} , {'Name':'tag:Name', 'Values':[instance_name_to_terminaite]}])
+instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']} , {'Name':'tag:Name', 'Values':[instance_name_to_terminaite]}])
 instance_ids = []
 
+
 for instance in instances:
-   print(instance.id, instance.instance_type)
+   print(instance.id, instance.instance_type )
+
 
 for instance in instances:
     instance_ids.append(instance.id)
